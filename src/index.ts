1806b1e9ddd245d571ace0e07a5fd15616e88711
pawooten@gmail.com <pawooten@gmail.com>
2024-03-17 10:58:31 -0400

    feat: phaser example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
-import { writeMessage } from "./util";
+import Phaser from "phaser";
 
-writeMessage();
-console.log('Enchanted Sandwich Adventure');
+// import iceBackgroundImage from '../assets/ice-background.png';
+// import cloudImage from '../assets/cloud.png';
+
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        // this.load.image('cloud', cloudImage);
+        // this.load.image('ice-background', iceBackgroundImage);
+    }
+    create ()
+    {
+        // this.add.image(400, 300, 'ice-background');
+        // this.add.image(200, 300, 'cloud');
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Example,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x:0, y: 200 }
+        }
+    }
+};
+
+const game = new Phaser.Game(config);
